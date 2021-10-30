5d098305e218eb27268dbf8958f4e27e415c9505
居木 <chenyingwei.cyw@antgroup.com>
2021-10-30 15:37:43 +0800

    Redux Fundamental UI And React

---
+++ src/features/todos/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector, shallowEqual } from 'react-redux'
+import TodoListItem from './TodoListItem'
+
+const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
+
+const TodoList = () => {
+  const todoIds = useSelector(selectTodoIds, shallowEqual)
+
+  // 使用 todoId 而不是直接传递 todo 对象，防止一个 TodoListItem 的 todo 更新导致全局的 TodoList rerender
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />
+  })
+
+  return <ul className="todo-list">{renderedListItems}</ul>
+}
+
+export default TodoList
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Provider } from 'react-redux'
 
 import './api/server'
 
@@ -15,8 +16,12 @@ store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
 console.log('State after dispatch: ', store.getState())
 
 ReactDOM.render(
+  // Render a `<Provider>` around the entire `<App>`,
+  // and pass the Redux store to as a prop
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
