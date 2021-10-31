0e7029a2011803861628fc57ddb9a28491faa945
居木 <chenyingwei.cyw@antgroup.com>
2021-10-31 21:50:48 +0800

    Standard Redux Patterns

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { useSelector, shallowEqual } from 'react-redux'
+import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
-const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
+import { selectFilteredTodoIds } from './todosSlice'
 
 const TodoList = () => {
-  const todoIds = useSelector(selectTodoIds, shallowEqual)
+  const todoIds = useSelector(selectFilteredTodoIds)
+  const loadingStatus = useSelector((state) => state.todos.status)
 
-  // 使用 todoId 而不是直接传递 todo 对象，防止一个 TodoListItem 的 todo 更新导致全局的 TodoList rerender
+  if (loadingStatus === 'loading') {
+    return (
+      <div className="todo-list">
+        <div className="loader" />
+      </div>
+    )
+  }
+
   const renderedListItems = todoIds.map((todoId) => {
     return <TodoListItem key={todoId} id={todoId} />
   })
